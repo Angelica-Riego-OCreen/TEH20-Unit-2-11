f7c83c17570fb5084a7189c7b125a57a6d8b9e5f
Angelica-Riego-OCreen <angelica.ocreen@stu.ocsb.ca>
2026-03-25 13:13:23 -0400

    made code

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Angelica O
+ * Created by: Angelica RO
  * Created on: Mar 2026
  * This program Compares numbers
 */
@@ -12,8 +12,29 @@ let Number2 : number = (0,99)
 // Press A
 input.onButtonPressed(Button.A, function() {
   basic.clearScreen()
+  basic.showString("#1:" )
   basic.showNumber(Number1)
+
+})
+
+// Press B
+input.onButtonPressed(Button.B, function() {
+  basic.clearScreen()
+  basic.showString("#2:" )
+  basic.showNumber(Number2)
   
 })
 
-
+// Shake
+input.onGesture(Gesture.Shake, function() {
+  
+  if (Number1 > Number2) { 
+    basic.showNumber(Number1)
+    basic.showString(">")
+    basic.showNumber(Number2)
+  } else {
+    basic.showNumber(Number1)
+    basic.showString("<")
+    basic.showNumber(Number2)
+}
+})
