c9079e11a19b2fd2c201515a7598c2361a023bfc
Angelica-Riego-OCreen <angelica.ocreen@stu.ocsb.ca>
2026-03-31 12:33:55 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -6,14 +6,14 @@
 */
 
 // creates variables
-let Number1 : number = (0,99)
-let Number2 : number = (0,99)
+const number1 : number = randint(0, 99)
+const number2 : number = randint(0,99)
 
 // Press A
 input.onButtonPressed(Button.A, function() {
   basic.clearScreen()
   basic.showString("#1:" )
-  basic.showNumber(Number1)
+  basic.showNumber(number1)
 
 })
 
@@ -21,20 +21,20 @@ input.onButtonPressed(Button.A, function() {
 input.onButtonPressed(Button.B, function() {
   basic.clearScreen()
   basic.showString("#2:" )
-  basic.showNumber(Number2)
+  basic.showNumber(number2)
   
 })
 
 // Shake
 input.onGesture(Gesture.Shake, function() {
   
-  if (Number1 > Number2) { 
-    basic.showNumber(Number1)
+  if (number1 > number2) { 
+    basic.showNumber(number1)
     basic.showString(">")
-    basic.showNumber(Number2)
+    basic.showNumber(number2)
   } else {
-    basic.showNumber(Number1)
+    basic.showNumber(number1)
     basic.showString("<")
-    basic.showNumber(Number2)
+    basic.showNumber(number2)
 }
 })
